8b5c04980d0af0dfed26fd1339a8e00f0678e19b
taka66 <taka66.iphone@gmail.com>
2025-04-18 01:31:46 +0900

    bugfix: adjust position

--- src/blocks/Card/CardHoverEffect/CardHoverEffect.tsx
+++ src/blocks/Card/CardHoverEffect/CardHoverEffect.tsx
@@ -20,7 +20,7 @@ export const HoverEffect = ({
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
-    <div className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10", className)}>
+    <div className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 py-0", className)}>
       {items.map((item, idx) => {
         const CardWrapper = item.link ? "a" : "div";
         const wrapperProps = item.link
