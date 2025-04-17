43acc1a1f3ad56dd372f44907e05be6768841dea
taka66 <taka66.iphone@gmail.com>
2025-04-17 18:50:32 +0900

    feat: add projects data

--- src/blocks/Card/CardHoverEffect/card-hover-effect.tsx
+++ src/blocks/Card/CardHoverEffect/card-hover-effect.tsx
@@ -12,7 +12,7 @@ export const HoverEffect = ({
   items: {
     title: string;
     description: string;
-    link: string;
+    link?: string;
   }[];
   className?: string;
 }) => {
@@ -20,31 +20,42 @@ export const HoverEffect = ({
 
   return (
     <div className={cn("grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3  py-10", className)}>
-      {items.map((item, idx) => (
-        <a href={item?.link} key={item?.link} className="relative group  block p-2 h-full w-full" onMouseEnter={() => setHoveredIndex(idx)} onMouseLeave={() => setHoveredIndex(null)}>
-          <AnimatePresence>
-            {hoveredIndex === idx && (
-              <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
-                layoutId="hoverBackground"
-                initial={{ opacity: 0 }}
-                animate={{
-                  opacity: 1,
-                  transition: { duration: 0.15 },
-                }}
-                exit={{
-                  opacity: 0,
-                  transition: { duration: 0.15, delay: 0.2 },
-                }}
-              />
-            )}
-          </AnimatePresence>
-          <Card>
-            <CardTitle>{item.title}</CardTitle>
-            <CardDescription>{item.description}</CardDescription>
-          </Card>
-        </a>
-      ))}
+      {items.map((item, idx) => {
+        const CardWrapper = item.link ? "a" : "div";
+        const wrapperProps = item.link
+          ? {
+              href: item.link,
+              target: "_blank",
+              rel: "noopener noreferrer",
+            }
+          : {};
+
+        return (
+          <CardWrapper key={idx} {...wrapperProps} className="relative group block p-2 h-full w-full" onMouseEnter={() => setHoveredIndex(idx)} onMouseLeave={() => setHoveredIndex(null)}>
+            <AnimatePresence>
+              {hoveredIndex === idx && (
+                <motion.span
+                  className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block rounded-3xl"
+                  layoutId="hoverBackground"
+                  initial={{ opacity: 0 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 0.15 },
+                  }}
+                  exit={{
+                    opacity: 0,
+                    transition: { duration: 0.15, delay: 0.2 },
+                  }}
+                />
+              )}
+            </AnimatePresence>
+            <Card>
+              <CardTitle>{item.title}</CardTitle>
+              <CardDescription>{item.description}</CardDescription>
+            </Card>
+          </CardWrapper>
+        );
+      })}
     </div>
   );
 };
@@ -58,9 +69,11 @@ export const Card = ({ className, children }: { className?: string; children: Re
     </div>
   );
 };
+
 export const CardTitle = ({ className, children }: { className?: string; children: React.ReactNode }) => {
   return <h4 className={cn("text-zinc-100 font-bold tracking-wide mt-4", className)}>{children}</h4>;
 };
+
 export const CardDescription = ({ className, children }: { className?: string; children: React.ReactNode }) => {
   return <p className={cn("mt-8 text-zinc-400 tracking-wide leading-relaxed text-sm", className)}>{children}</p>;
 };
