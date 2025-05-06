9e771320a291abd324dd1cc8c72d9871470a8fc8
taka66 <taka66.iphone@gmail.com>
2025-05-07 01:02:44 +0900

    chore: update padding

--- src/app/[lang]/design/page.tsx
+++ src/app/[lang]/design/page.tsx
@@ -26,7 +26,7 @@ export default async function Design(props: DesignPageProps) {
   const projects: DesignProject[] = dict.DesignPage.projects as DesignProject[];
 
   return (
-    <MotionWrapper className="max-w-5xl mx-auto px-8 pt-24">
+    <MotionWrapper className="max-w-5xl mx-auto px-8 pt-8">
       <div className="mb-12">
         <h3 className="text-3xl font-bold mb-3">{dict.DesignPage.heading}</h3>
         <p className="text-gray-700 dark:text-gray-300">{dict.DesignPage.subheading}</p>
